e8577548adb4176731be5e337560d8bae9cc63c5
bloges56 <brady.drew.logan@gmail.com>
2022-03-29 12:21:21 -0400

    bug fix

--- Assets/Scripts/Monobehaviours/Player.cs
+++ Assets/Scripts/Monobehaviours/Player.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    //interactable object that player is in range of
     private Interactable interact;
+
+    //tracks if player is currently interacting with something
     private bool interacting = false;
+
     private void Update()
     {
+        //if there is an object in range and when the player hits E
         if(interact != null && Input.GetKeyDown(KeyCode.E))
         {
+            //if not currently interacting, interact
             if(!interacting)
             {
                 interact.interact();
                 interacting = true;
             }
+            //if interacting, exit interaction
             else
             {
                 interact.exitInteract();
@@ -27,6 +33,7 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //check if collision is an interactable 
         if (collision.gameObject.CompareTag("Interactable"))
         {
             Interactable hitObject = collision.gameObject.GetComponent<Interactable>();
