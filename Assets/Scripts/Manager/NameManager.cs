01bbe1e912b649f8db7f3c31e3a6ab1804fc661b
Lillian Milano <limilano@davidson.edu>
2022-04-28 22:33:21 -0400

    tutorial added to SetName

--- Assets/Scripts/Manager/NameManager.cs
+++ Assets/Scripts/Manager/NameManager.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Yarn.Unity;
 
 
 public class NameManager : MonoBehaviour
 {
-
+    static NameManager nameManager;
     [SerializeField] TextMeshProUGUI charName;
     GameObject diaText;
     public Canvas inputCanvas;
     public Button doneButton;
+    public GameObject lastDialogue;
 
     public string sceneToLoadNext = "Arcade";
 
@@ -24,34 +26,43 @@ public class NameManager : MonoBehaviour
 
     void Start()
     {  
+        nameManager = GameObject.Find("NameInputManager").GetComponent<NameManager>();
         doneButton = GameObject.Find("DoneButton").GetComponent<Button>();
         inputCanvas = GameObject.Find("NameEnter").GetComponent<Canvas>();
+        lastDialogue = GameObject.Find("Text");
+        doneButton.gameObject.SetActive(false);
+        inputCanvas.gameObject.SetActive(false);
         // diaText = GameObject.Find("DialogueSystem");
 
-        inputCanvas.gameObject.SetActive(askForName); 
+        //inputCanvas.gameObject.SetActive(askForName); 
         // diaText.gameObject.SetActive(!askForName);
 
         sceneManager = Managers.sceneManager;
         gameManager = Managers.gameManager; 
 
-        doneButton.onClick.AddListener(DoneName);
+        //doneButton.onClick.AddListener(DoneName);
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputCanvas.gameObject.SetActive(askForName);
+        //inputCanvas.gameObject.SetActive(askForName);
         // diaText.gameObject.SetActive(!askForName);
 
-        if(Input.GetKeyDown(KeyCode.Period)){
+        if(setName == true) {
+            doneButton.gameObject.SetActive(true);
+            inputCanvas.gameObject.SetActive(true);
+        }
+
+        if(setName == true && Input.GetKeyDown(KeyCode.Period)){
             askForName = !askForName;
         }
 
-        if(Input.GetKeyDown(KeyCode.B)){
+        if(setName == true && Input.GetKeyDown(KeyCode.B)){
             Debug.Log("Name is: "+gameManager.GetName());
         }
 
-        if(Input.GetKeyDown(KeyCode.Return)){
+        if(setName == true && Input.GetKeyDown(KeyCode.Return)){
             gameManager.SetName(charName.text);
             askForName = false;
             setName = true;
@@ -64,5 +75,10 @@ public class NameManager : MonoBehaviour
         gameManager.SetName(charName.text);
         sceneManager.SingleLoad(sceneToLoadNext);
 
+    }
+
+    [YarnCommand("SetNameTrue")]
+    public static void SetNameTrue() {
+       nameManager.setName = true;
     }
 }
